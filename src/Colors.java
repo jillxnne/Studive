cd003a6cc7213400d295870aeb0a6544c99fa3ab
Jillanne <jillxnne@gmail.com>
2024-10-15 09:40:52 +0200

    ok

--- src/Colors.java
+++ src/Colors.java
@@ -14,33 +14,21 @@ public class Colors {
         this.colors[4] = p5.color(246,224,181);
     }
 
-    // Getter del número de colors
     int getNumColors(){
         return this.colors.length;
     }
-
-    // Getter del color primari
     int getFirstColor(){
         return  this.colors[0];
     }
-
-    // Getter del color secundari
     int getSecondColor(){
         return  this.colors[1];
     }
-
-    // Getter del color terciari
     int getThirdColor(){
         return  this.colors[2];
     }
-
-    // Getter del color i-èssim
     int getColorAt(int i){
         return this.colors[i];
     }
-
-
-    // Dibuixa paleta de colors
     void displayColors(PApplet p5, float x, float y, float w){
         p5.pushStyle();
         //Llegenda
